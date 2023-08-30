671d376c24cff3baf97ef689b3d87c0fea295601
Manuel26190 <manuelfaure26190@gmail.com>
2023-08-30 18:05:18 +0200

    save

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,25 @@
+import React from 'react';
 import '../../styles/HomePage/HomePage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-
-function HomePage () {
+function HomePage() {
     return (
         <section className='homePage-section'>
             <div className='picture-div'>
                 <img className='photo' src={require('../../assets/image.webp')} alt="cliché de moi" />
-            </div>                
+            </div>
             <div className='text-div'>
                 <h1>Bonjour, je suis <span className='h1-first-name'>Manuel</span><span className="home-hand">🤚</span></h1>
                 <h2>Je suis développeur Front-end.</h2>
+                <div className='social-icons'>
+                    <a href="lien_vers_github"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a href="lien_vers_linkedin"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a href="lien_vers_twitter"><FontAwesomeIcon icon={faTwitter} /></a>
+                </div>
             </div>
-            
-            
         </section>
     );
-};
+}
 
 export default HomePage;
-
