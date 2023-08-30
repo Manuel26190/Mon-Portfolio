2b982ded9b7d5ee4a87c5b134c9391b1bbf1460a
Manuel26190 <manuelfaure26190@gmail.com>
2023-08-30 18:33:53 +0200

    save

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,24 @@
+import React from 'react';
+import '../../styles/Contact/Contact.css';
 
-function Contact () {
+function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='form-div'>
+            <h1>Contactez-moi</h1>
+            <form>
+                <label htmlFor="name"></label>
+                <input type="text" id="name" name="name" placeholder="Entrez votre nom" />
+
+                <label htmlFor="email"></label>
+                <input type="email" id="email" name="email" placeholder="Entrez votre email" />
+
+                <label htmlFor="message"></label>
+                <textarea id="message" name="message" placeholder="Entrez votre message"></textarea>
+
+                <button type="submit">Envoyer</button>
+            </form>
         </div>
     );
-};
+}
 
 export default Contact;
