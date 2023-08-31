3197d300e168de904045edf02e5da0ae1a887ac6
Manuel26190 <manuelfaure26190@gmail.com>
2023-08-31 12:15:19 +0200

    save

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,18 +4,18 @@ import '../../styles/Contact/Contact.css';
 function Contact() {
     return (
         <div className='form-div'>
-            <h1>Contactez-moi</h1>
-            <form>
+            <h1 className='contact-title'>Contactez-moi</h1>
+            <form className='contact-form'>
                 <label htmlFor="name"></label>
-                <input type="text" id="name" name="name" placeholder="Entrez votre nom" />
+                <input type="text" id="name" name="name" className='form-input' placeholder="Entrez votre nom" />
 
                 <label htmlFor="email"></label>
-                <input type="email" id="email" name="email" placeholder="Entrez votre email" />
+                <input type="email" id="email" name="email" className='form-input' placeholder="Entrez votre email" />
 
                 <label htmlFor="message"></label>
-                <textarea id="message" name="message" placeholder="Entrez votre message"></textarea>
+                <textarea id="message" name="message" className='form-textarea' placeholder="Entrez votre message"></textarea>
 
-                <button type="submit">Envoyer</button>
+                <button type="submit" className='form-button'>Envoyer</button>
             </form>
         </div>
     );
