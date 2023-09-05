26233e31b6f4833624c4f0e37fdc19bacc694160
Manuel26190 <manuelfaure26190@gmail.com>
2023-09-05 14:09:52 +0200

    save

--- src/pages/AboutMe/Terminal.jsx
+++ src/pages/AboutMe/Terminal.jsx
@@ -3,17 +3,16 @@ import './Terminal.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
-function Terminal () {
+function Terminal ({text}) {
     return (
         <section className="terminal-section">
             <div className="banner-div">
-                <FontAwesomeIcon className="red-icon" icon={faCircle} />
-                <FontAwesomeIcon className="yellow-icon" icon={faCircle} />
-                <FontAwesomeIcon className="green-icon" icon={faCircle} />
+                <FontAwesomeIcon className="icon red" icon={faCircle} />
+                <FontAwesomeIcon className="icon yellow" icon={faCircle} />
+                <FontAwesomeIcon className="icon green" icon={faCircle} />
             </div>
             <div className="terminal-div">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus in consectetur animi a quibusdam, explicabo corporis neque nesciunt! Dolores recusandae quos molestiae ipsa exercitationem reprehenderit explicabo quisquam ratione mollitia.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta possimus in consectetur animi a quibusdam, explicabo corporis neque nesciunt! Dolores recusandae quos molestiae ipsa exercitationem reprehenderit explicabo quisquam ratione mollitia.</p>
+                <p>{text}</p>                               
             </div>
         </section>
     )
