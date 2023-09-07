a170e4ae240250030516a3bdf0efbd314904c0b0
Manuel Faure <manuelfaure26190@gmail.com>
2023-09-07 10:19:15 +0200

    save

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,21 @@
 
-function Portfolio () {
+import React from 'react';
+import PortfolioBlock from './PortfolioBlock';
+import {Box, Grid} from "@mui/material";
+import {info} from "../../info/info";
+
+export default function Portfolio() {
+
+    // console.log('info', info);
     return (
-        <div>
-            <h1>Portfolio</h1>
-        </div>
+        <Box>
+            <Grid container display={'flex'} justifyContent={'center'}>
+                {info.portfolio.map((project, index) => (
+                   <Grid item xs={12} md={6} key={index}>
+                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
+                   </Grid>
+                ))}
+            </Grid>
+        </Box>
     );
 };
-
-export default Portfolio;
