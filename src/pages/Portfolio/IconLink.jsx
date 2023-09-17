6ff597253bddafee9dfeea90cccdea9d61a93b98
Manuel26190 <manuelfaure26190@gmail.com>
2023-09-17 17:35:14 +0200

    save

--- src/pages/Portfolio/IconLink.jsx
+++ src/pages/Portfolio/IconLink.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const color = {
+   background: 'linear-gradient(135deg, rgb(0, 255, 164), rgb(166, 104, 255))',
+   WebkitBackgroundClip: 'text',
+   WebkitTextFillColor: 'transparent',
+ };
+
 
 function IconLink(props) {
    const { link, title, icon } = props;
    return (
-      <a href={link} target="_blank" rel="noopener noreferrer">
+      <a href={link} style={color} target="_blank" rel="noopener noreferrer">
          <FontAwesomeIcon icon={icon} />
          {title}
       </a>
--- src/pages/Portfolio/PortfolioBlock.jsx
+++ src/pages/Portfolio/PortfolioBlock.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import IconLink from "./IconLink";
 import {Box} from "@mui/material";
 import { faLaptop, faCode } from '@fortawesome/free-solid-svg-icons';
-import './PortfolioBlock.css';
+import './PortfolioBlockBis.css';
 
 
 function PortfolioBlock(props) {
    const {image, live, source, title,} = props;
    return (
-      <Box className={'box-first-container'} >
-         <h1>{title}</h1>
+      <Box className={'box-first-container'} >         
+         <a href="http://www.thedresscounter.com/" target="_blank">
+  <div class="container">
+    <div class="background-img">
+      <div class="box">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="content">
+        <h2>{title}</h2>
+          <p><a>All our modules are designed to play nicely with responsive of course. At the end of the day it comes down to the theme you use - our code doesn't get in your way.</a></p>
+        </div>        
+      </div>
+    </div>
+  </div>
+
+  </a>
          <Box  component={'img'} className='box-image' src={image} alt={'mockup'}/>         
          <Box className={'portfolio'} py={'2rem'}>
             <Box className={'icon-box'} p={1}>
