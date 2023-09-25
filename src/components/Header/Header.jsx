1726efaf6c643c64dd3aadd1cd7719b94a5df3bd
Manuel26190 <manuelfaure26190@gmail.com>
2023-09-25 22:01:50 +0200

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ function Header () {
         <div className="div-header">           
             <nav>
                 <ul>
-                    <li><Link to="/" className={currentPage === '/'? 'nav-link active faHouse' : 'nav-link faHouse'}><FontAwesomeIcon icon={faHouse} /></Link></li>
+                    <li><Link to="/home-portfolio" className={currentPage === '/'? 'nav-link active faHouse' : 'nav-link faHouse'}><FontAwesomeIcon icon={faHouse} /></Link></li>
                     <li><Link to="/about" className={currentPage === '/about'? 'nav-link active' : 'nav-link'}>A propos de moi</Link></li>
                     {/* <li><Link to="/" style={{fontSize: "3em"}} className={currentPage === '/'? 'nav-link active faHouse' : 'nav-link faHouse'}>mf</Link></li> */}
                     <li><Link to="/portfolio" className={currentPage === '/portfolio'? 'nav-link active' : 'nav-link'}>Portfolio</Link></li>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './styles/index.css';
 import './index.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import Header from './components/Header/Header';
