90cfaf0dacbe08029037ff71851489212e8779da
Manuel26190 <manuelfaure26190@gmail.com>
2023-12-06 18:14:46 +0100

    save

--- src/pages/Portfolio/Modal.jsx
+++ src/pages/Portfolio/Modal.jsx
@@ -6,10 +6,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-
-function Modal(props) {
+function Modal() {
     const [currentIndex, setCurrentIndex] = useState(0);
-    //const { live } = props;
+    const [isOpen, setOpen] = useState(true); // Initialisez isOpen à true si vous voulez que la modal soit ouverte par défaut
 
     const handlePreviousClick = () => {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + info.portfolio[4].live.length) % info.portfolio[4].live.length);
@@ -17,10 +16,15 @@ function Modal(props) {
 
     const handleNextClick = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % info.portfolio[4].live.length);
-    };
+    };  
+    
+    const handleClose = () => {        
+        setOpen(!isOpen)        
+    };    
 
     return (
-        <div className="modal-container">
+        <div className={`modal-container ${isOpen ? 'open' : 'close'}`}>
+            <button className="close-btn" onClick={handleClose}>Fermer</button>
             <button className="preview-btn" onClick={handlePreviousClick}>
                 <FontAwesomeIcon className="chevron-icon" icon={faChevronLeft} />
             </button>
--- src/pages/Portfolio/PortfolioBlock.jsx
+++ src/pages/Portfolio/PortfolioBlock.jsx
@@ -1,63 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
 import IconLink from "./IconLink";
-import {Box} from "@mui/material";
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { Box } from "@mui/material";
+import { faCode, faDisplay, faL } from '@fortawesome/free-solid-svg-icons';
 import './PortfolioBlock.css';
 import Modal from './Modal';
 
-
 function PortfolioBlock(props) {
-   const {image, live, source, title, description} = props;   
+   const { image, live, source, title, description } = props;
 
-     if (!live || live.length === 4){
-        return (
-            <Box className={'box-first-container'} >         
-            <a className='project-link' href={live} target="_blank" rel='noreferrer'>            
+   const [isOpen, setOpen] = useState(false);
+
+   const handleOpen = () => {     
+      setOpen(!isOpen);     
+   };
+
+   if (!live || live.length === 4) {
+      return (
+         <Box className={'box-first-container'}>
+            <a className='project-link' onClick={handleOpen}>
                <div className="container">
                   <div className="background-img" style={{ backgroundImage: `url(${image})` }}>
-                     <div className="box">                  
+                     <div className="box">
                         <span className='span-text'></span>
                         <div className="content">
                            <p className='portfolio-title'>{title}</p>
                            <p className='portfolio-descritpion'>{description}</p>
-                        </div>        
+                        </div>
                      </div>
                   </div>
                </div>
             </a>
-            <Modal />                  
-            <Box className={'portfolio'} py={'2rem'}>            
+            {isOpen && <Modal />}
+            <Box className={'portfolio'} py={'2rem'}>
                <Box className={'icon-box'} p={1} >
                   <IconLink link={source} title={'Source Code'} icon={faCode} />
                </Box>
             </Box>
          </Box>
-        ) 
-      } else {
-         return (
-            <Box className={'box-first-container'} >         
-               <a href={live} target="_blank" rel='noreferrer'>
-               
+      )
+   } else {
+      return (
+         <Box className={'box-first-container'}>
+            <a href={live} target="_blank" rel='noreferrer'>
                <div className="container">
                   <div className="background-img" style={{ backgroundImage: `url(${image})` }}>
-                     <div className="box">                  
+                     <div className="box">
                         <span className='span-text'></span>
                         <div className="content">
                            <p className='portfolio-title'>{title}</p>
                            <p className='portfolio-descritpion'>{description}</p>
-                        </div>        
+                        </div>
                      </div>
                   </div>
                </div>
-               </a>                  
-               <Box className={'portfolio'} py={'2rem'}>            
-                  <Box className={'icon-box'} p={1} >
-                     <IconLink link={source} title={'Source Code'} icon={faCode} />
-                  </Box>
+            </a>
+            <Box className={'portfolio'} py={'2rem'}>
+               <Box className={'icon-box'} p={1} >
+                  <IconLink link={source} title={'Source Code'} icon={faCode} />
                </Box>
             </Box>
-         );
-      }   
+         </Box>
+      );
+   }
 }
 
 export default PortfolioBlock;
