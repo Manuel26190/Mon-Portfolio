b4ed9037317687e38c17dd11751197c5f1b9de32
Manuel26190 <manuelfaure26190@gmail.com>
2023-12-07 18:37:22 +0100

    save

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './HomePage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function HomePage() {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const links = document.querySelectorAll('a');
+      links.forEach(link => {
+        link.style.color = isDarkMode ? '#1f1f1f' : '#f8f8f8';
+      });
+
     return (
         <section className='homePage-section'>
             <div className='picture-div'>
@@ -16,8 +24,8 @@ function HomePage() {
                     <li className="text-li"><span className="text-li-icon" aria-label="cheese" role="img">☕</span> Carburant : le café</li>
                     <li className="text-li"><span className="text-li-icon" aria-label="cheese" role="img">🌎</span> Réside dans la Drôme (fr)</li>
                     <li className="text-li"><span className="text-li-icon" aria-label="cheese" role="img">💼</span> Formé chez OpenClassrooms</li>
-                    <li className="text-li"><span className="text-li-icon" aria-label="cheese" role="img">📧</span> manuelfaure26190@gmail.com</li>
-                </ul>
+                    <a href="mailto:manuelfaure26190@gmail.com" ><li className="text-li"><span className="text-li-icon" aria-label="cheese" role="img">📧</span> manuelfaure26190@gmail.com</li></a>
+                    </ul>
                 <div className='social-icons'>
                     <a href="https://github.com/Manuel26190" target='_blank' rel="noreferrer" ><FontAwesomeIcon icon={faGithub} /></a>
                     <a href="https://www.linkedin.com/in/manuel-faure-6a7a96218/" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
